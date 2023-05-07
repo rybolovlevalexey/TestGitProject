9de429bb9f1c58c2265930872bace520f8439196
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-05-07 11:08:34 +0300

    test commit

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -7,6 +7,7 @@ namespace TestGitProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello TestGitProject!");
         }
     }
 }
