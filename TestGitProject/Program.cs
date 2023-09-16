0bda65ad461c7348225e1ddc6675400e24162a4f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-09-16 18:02:42 +0300

    тестирование программы с пользователями их записями, реализованной с использованием LINQ

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -15,7 +15,69 @@ namespace TestGitProject
     {
         static void Main(string[] args)
         {
+            BusinessLogic bl = new BusinessLogic();
 
+            var req1 = bl.GetUsersBySurname("Begging");
+            foreach (var u in req1)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            var req2 = bl.GetUsersBySurname("NULL");
+            foreach (var u in req2)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            Console.WriteLine(bl.GetUserByID(1));
+            Console.WriteLine();
+
+            var req3 = bl.GetUsersBySubstring("a");
+            foreach (var u in req3)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            var req4 = bl.GetUsersBySubstring("Hell yeah");
+            foreach (var u in req4)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            var req5 = bl.GetAllUniqueNames();
+            foreach (var u in req5)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            var req6 = bl.GetAllAuthors();
+            foreach (var u in req6)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            var req7 = bl.GetUsersDictionary();
+            foreach (var kvl in req7)
+                Console.WriteLine($"{kvl.Key} - {kvl.Value}");
+            Console.WriteLine();
+
+            var req8 = bl.GetMaxID();
+            Console.WriteLine(req8);
+            Console.WriteLine();
+
+            var req9 = bl.GetOrderedUsers();
+            foreach (var u in req9)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            var req10 = bl.GetDescendingOrderedUsers();
+            foreach (var u in req10)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            var req11 = bl.GetReversedUsers();
+            foreach (var u in req11)
+                Console.WriteLine(u);
+            Console.WriteLine();
+
+            var req12 = bl.GetUsersPage(5, 1);
+            foreach (var u in req12)
+                Console.WriteLine(u);
+            Console.WriteLine();
         }
 
     }
@@ -48,7 +110,23 @@ namespace TestGitProject
         private List<Record> records = new List<Record>();
         public BusinessLogic()
         {
-            // наполнение обеих коллекций тестовыми данными
+            users.Add(new User(0, "Al", "Bal"));
+            users.Add(new User(1, "Twen", "Folaks"));
+            users.Add(new User(2, "Frodo", "Begging"));
+            users.Add(new User(3, "Frodo", "Begging"));
+            users.Add(new User(4, "Frodo", "Begging"));
+            users.Add(new User(5, "Frodo", "Begging"));
+            users.Add(new User(6, "Frodo", "Begging"));
+            users.Add(new User(7, "Frodo", "Begging"));
+            users.Add(new User(8, "Frodo", "Begging"));
+            users.Add(new User(9, "Frodo", "Begging"));
+            users.Add(new User(10, "Frodo", "Begging"));
+
+
+
+            records.Add(new Record(users[0], "First message"));
+            records.Add(new Record(users[1], "2nd message"));
+            records.Add(new Record(users[1], "Third message"));
         }
 
         public List<User> GetUsersBySurname(String surname)
