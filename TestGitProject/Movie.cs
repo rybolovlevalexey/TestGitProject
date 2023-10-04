486d3367696915e46f598c9153378c7a9154bacd
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-10-04 19:43:30 +0300

    создан класс Movie с необходимыми методами

---
+++ TestGitProject/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGitProject
+{
+    class Movie
+    {
+        public string name;
+        public string rating;
+        public string id;
+
+        private HashSet<string> actors = new HashSet<string>();
+        private string director;
+        private HashSet<string> tags = new HashSet<string>();
+
+        public Movie(string name, string id, HashSet<string> actors=null, string director=null, HashSet<string> tags=null, string rating=null) {
+            this.name = name;
+            this.actors = actors;
+            this.director = director;
+            this.tags = tags;
+            this.rating = rating;
+            this.id = id;
+        }
+
+        public void add_actor(string name_actor)
+        {
+            this.actors.Add(name_actor);
+        }
+        public void add_tags(string tag_name) { this.tags.Add(tag_name); }
+        public void change_director(string director_name) { this.director = director_name; }
+    }
+}
--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -8,6 +8,8 @@ using System.IO;
 using System.Text;
 using System.Xml;
 using System.Xml.XPath;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
 
 namespace TestGitProject
 {
@@ -15,12 +17,32 @@ namespace TestGitProject
     {
         static void Main(string[] args)
         {
-            IEnumerable<int> numbers = Enumerable.Range(0, 10000000);
-            List<int> result = (from num in numbers.AsParallel() where 
-                                Enumerable.Range(1, num / 2 + 1).Where(d => num % d == 0).Sum() == num select num).ToList();
-            foreach (var elem in result)
-                Console.WriteLine(elem);
-        }
+            Dictionary<string, Movie> films = new Dictionary<string, Movie>();  // название: фильм
+            Dictionary<string, Movie> people = new Dictionary<string, Movie>();  // имя учатсника: фильм
+            Dictionary<string, Movie> tags_dict = new Dictionary<string, Movie>();  // тэг: фильм
+            string dataset_path = @"C:\Универ\ml-latest";
 
+            string[] MovieCodes_IMDB = File.ReadAllLines(dataset_path + "MovieCodes_IMDB.tsv")[1..];
+            // titleId (нужен), ordering, title(нужен), region(RU или US), language(RU или US), types, attributes, isOriginalTitle; делятся табами
+            foreach (var line in MovieCodes_IMDB)
+            {
+                string[] elements = line.Split('\t');
+                string film_id = elements[0], title = elements[2], region = elements[3], language = elements[4];
+                if (region == "RU" || region == "US" || language == "RU" || language == "US")
+                {
+                    if (!films.ContainsKey(title))
+                    {
+                        films[title] = new Movie()
+                    }
+                }
+            }
+
+            string[] ActorsDirectorsNames_IMDB = File.ReadAllLines(dataset_path + "ActorsDirectorsNames_IMDB.txt"); 
+            // 
+            string[] Ratings_IMDB = File.ReadAllLines(dataset_path + "Ratings_IMDB.tsv"); // 
+            string[] links_IMDB_MovieLens = File.ReadAllLines(dataset_path + "links_IMDB_MovieLens.csv"); // 
+            string[] TagCodes_MovieLens = File.ReadAllLines(dataset_path + "TagCodes_MovieLens.csv"); // 
+            string[] TagScores_MovieLens = File.ReadAllLines(dataset_path + "TagScores_MovieLens.csv"); // 
+        }
     }
 }
