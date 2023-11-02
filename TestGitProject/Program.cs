e015e34e52611a33064a610c504e82302053b4b4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-11-02 11:13:43 +0300

    использование асинхронности не увенчалось успехом

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -13,6 +13,7 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Collections.Concurrent;
 
 namespace TestGitProject
 {
@@ -23,10 +24,13 @@ namespace TestGitProject
         static Dictionary<string, List<Movie>> tags_dict = new Dictionary<string, List<Movie>>();  // тэг: фильмы
         static string dataset_path = @"C:\Универ\ml-latest\";
 
-        static void Main(string[] args)
+        static Dictionary<string, Person> persons_for_async = new Dictionary<string, Person>();
+        static Dictionary<string, List<string>> tags_async_result = new Dictionary<string, List<string>>();
+
+        static async Task Main(string[] args)
         {
             Dictionary<string, List<string>> id_name = new Dictionary<string, List<string>>();  // id фильма: название фильма
-
+            
             // наполнение словарей films и id_name
             using (StreamReader reader = new StreamReader(dataset_path + "MovieCodes_IMDB.tsv"))
             {
@@ -72,61 +76,120 @@ namespace TestGitProject
             }
             Console.WriteLine("Rating done.");
 
-
-            Dictionary<string, List<string>> result_tags = make_tags(id_name);
-            foreach (var film_name in result_tags.Keys.AsParallel())
+            bool flag_tags_people_async = true;
+            
+            if (flag_tags_people_async)
             {
-                films[film_name].tags = result_tags[film_name].ToHashSet<string>();
-            }
-            Console.WriteLine("Make tags done.");
-
-            // наполнение фильмов их актёрами и режиссёрами
-            Dictionary<string, Person> result_people = make_people(id_name);
-            foreach (var per in result_people.Values)
-            {
-                foreach (var movie_id in per.actor_movis_id)
+                await Task.WhenAll(make_people_async(id_name), make_tags_async(id_name));
+                foreach (var film_name in tags_async_result.Keys.AsParallel())
                 {
-                    if (!id_name.ContainsKey(movie_id))
-                        continue;
-                    foreach (var movie_name in id_name[movie_id])
-                        films[movie_name].actors.Add(per.name);
+                    films[film_name].tags = tags_async_result[film_name].ToHashSet<string>();
                 }
-                foreach (var movie_id in per.director_movies_id)
-                {
-                    if (!id_name.ContainsKey(movie_id))
-                        continue;
-                    foreach (var movie_name in id_name[movie_id])
-                        films[movie_name].directors.Add(per.name);
-                }
-            }
-            Console.WriteLine("Make people done");
+                Console.WriteLine("Make tags done.");
 
-            // наполнение второго словаря
-            foreach (var per in result_people.Values)
-            {
-                if (!people.ContainsKey(per.name))
+                foreach (var per in persons_for_async.Values)
+                {
+                    foreach (var movie_id in per.actor_movis_id)
+                    {
+                        if (!id_name.ContainsKey(movie_id))
+                            continue;
+                        foreach (var movie_name in id_name[movie_id])
+                            films[movie_name].actors.Add(per.name);
+                    }
+                    foreach (var movie_id in per.director_movies_id)
+                    {
+                        if (!id_name.ContainsKey(movie_id))
+                            continue;
+                        foreach (var movie_name in id_name[movie_id])
+                            films[movie_name].directors.Add(per.name);
+                    }
+                }
+                Console.WriteLine("Make people done");
+
+                // финальные приготовления
+                // наполнение второго словаря
+                foreach (var per in persons_for_async.Values)
+                {
                     people[per.name] = new List<Movie>();
-                foreach (var mov_id in per.movies_id)
-                {
-                    if (!id_name.ContainsKey(mov_id))
-                        continue;
-                    foreach (var mov_name in id_name[mov_id])
-                        people[per.name].Add(films[mov_name]);
+                    foreach (var mov_id in per.movies_id)
+                    {
+                        if (!id_name.ContainsKey(mov_id))
+                            continue;
+                        foreach (var mov_name in id_name[mov_id])
+                            people[per.name].Add(films[mov_name]);
+                    }
                 }
-            }
-            Console.WriteLine("Dictionary with persons is ready.");
+                Console.WriteLine("Dictionary with persons is ready.");
 
-            // наполнение третьего словаря, когда все классы фильмов заполнены
-            foreach (var film_name in result_tags.Keys.AsParallel())
-            {
-                foreach (var tag in result_tags[film_name])
+                // наполнение третьего словаря, когда все классы фильмов заполнены
+                foreach (var film_name in tags_async_result.Keys.AsParallel())
                 {
-                    if (!tags_dict.ContainsKey(tag))
-                        tags_dict[tag] = new List<Movie>();
-                    tags_dict[tag].Add(films[film_name]);
+                    foreach (var tag in tags_async_result[film_name])
+                    {
+                        if (!tags_dict.ContainsKey(tag))
+                            tags_dict[tag] = new List<Movie>();
+                        tags_dict[tag].Add(films[film_name]);
+                    }
                 }
+                Console.WriteLine("Dictionary with tags is ready.");
             }
-            Console.WriteLine("Dictionary with tags is ready.");
+            else
+            {
+                Dictionary<string, List<string>> result_tags = make_tags(id_name);
+                foreach (var film_name in result_tags.Keys.AsParallel())
+                {
+                    films[film_name].tags = result_tags[film_name].ToHashSet<string>();
+                }
+                Console.WriteLine("Make tags done.");
+
+                // наполнение фильмов их актёрами и режиссёрами
+                Dictionary<string, Person> result_people = make_people(id_name);
+                foreach (var per in result_people.Values)
+                {
+                    foreach (var movie_id in per.actor_movis_id)
+                    {
+                        if (!id_name.ContainsKey(movie_id))
+                            continue;
+                        foreach (var movie_name in id_name[movie_id])
+                            films[movie_name].actors.Add(per.name);
+                    }
+                    foreach (var movie_id in per.director_movies_id)
+                    {
+                        if (!id_name.ContainsKey(movie_id))
+                            continue;
+                        foreach (var movie_name in id_name[movie_id])
+                            films[movie_name].directors.Add(per.name);
+                    }
+                }
+                Console.WriteLine("Make people done");
+
+                // финальные приготовления
+                // наполнение второго словаря
+                foreach (var per in result_people.Values)
+                {
+                    people[per.name] = new List<Movie>();
+                    foreach (var mov_id in per.movies_id)
+                    {
+                        if (!id_name.ContainsKey(mov_id))
+                            continue;
+                        foreach (var mov_name in id_name[mov_id])
+                            people[per.name].Add(films[mov_name]);
+                    }
+                }
+                Console.WriteLine("Dictionary with persons is ready.");
+
+                // наполнение третьего словаря, когда все классы фильмов заполнены
+                foreach (var film_name in result_tags.Keys.AsParallel())
+                {
+                    foreach (var tag in result_tags[film_name])
+                    {
+                        if (!tags_dict.ContainsKey(tag))
+                            tags_dict[tag] = new List<Movie>();
+                        tags_dict[tag].Add(films[film_name]);
+                    }
+                }
+                Console.WriteLine("Dictionary with tags is ready.");
+            }
 
 
             while (true)
@@ -309,6 +372,115 @@ namespace TestGitProject
             Console.WriteLine("Make people 2/2 done.");
 
             return persons;
+        }
+
+        static async Task make_tags_async(Dictionary<string, List<string>> films_id_name)
+        {
+            Console.WriteLine("Start make tags async.");
+            Dictionary<string, string> MovLens_IMDB = new Dictionary<string, string>();
+            Dictionary<string, string> tag_dict = new Dictionary<string, string>(); // tag_id: tag
+
+
+            using (StreamReader reader = new StreamReader(dataset_path + "links_IMDB_MovieLens.csv"))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    string line = await reader.ReadLineAsync();
+                    int one = line.IndexOf(",");
+                    MovLens_IMDB[line[0..one]] = "tt" + line[(one + 1)..(line.IndexOf(",", one + 1))];
+                }
+            }
+            Console.WriteLine("Make tags done 1/3.");
+
+
+            using (StreamReader reader = new StreamReader(dataset_path + "TagCodes_MovieLens.csv"))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    string line = await reader.ReadLineAsync();
+                    int zero = line.IndexOf(",");
+                    tag_dict[line[0..zero]] = line[(zero + 1)..];
+                }
+            }
+            Console.WriteLine("Make tags done 2/3.");
+
+            using (StreamReader reader = new StreamReader(dataset_path + "TagScores_MovieLens.csv"))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    string line = await reader.ReadLineAsync();
+                    int zero = line.IndexOf(","), one = line.IndexOf(",", zero + 1);
+                    string tagid = line[(zero + 1)..one], rel = line[(one + 1)..], MovLens_id = line[0..zero];
+
+                    string IMDB_id = MovLens_IMDB[MovLens_id];
+                    int relevants;
+                    if (rel.Length < 4)
+                        rel += "0";
+                    relevants = Convert.ToInt32(rel.Substring(2, 2));
+                    if (relevants > 50)
+                    {
+                        if (!films_id_name.ContainsKey(IMDB_id))
+                            continue;
+                        foreach (var film_name in films_id_name[IMDB_id])
+                        {
+                            if (!tags_async_result.ContainsKey(film_name))
+                                tags_async_result[film_name] = new List<string>();
+                            tags_async_result[film_name].Add(tag_dict[tagid]);
+                        }
+
+                    }
+                }
+            }
+            Console.WriteLine("Make tags done 3/3.");
+        }
+        static async Task make_people_async(Dictionary<string, List<string>> films_id_name)
+        {
+            Console.WriteLine("Start make people async.");
+            // Dictionary<string, Person> persons = new Dictionary<string, Person>();  // id: Person
+
+            using (StreamReader reader = new StreamReader(dataset_path + "ActorsDirectorsNames_IMDB.txt"))
+            {
+                await reader.ReadLineAsync();
+                while (!reader.EndOfStream)
+                {
+                    string line = await reader.ReadLineAsync();
+                    int zero = line.IndexOf("\t"), one = line.IndexOf("\t", zero + 1), two = line.IndexOf("\t", one + 1),
+                        three = line.IndexOf("\t", two + 1), four = line.IndexOf("\t", three + 1);
+                    string per_id = line[0..zero], per_name = line[(zero + 1)..one], professions = line[(three + 1)..four];
+
+                    if (professions.Contains("director") || professions.Contains("actor") || professions.Contains("actress"))
+                        persons_for_async[per_id] = new Person(per_name, per_id);
+                }
+            }
+            Console.WriteLine("Make people 1/2 done.");
+
+
+            using (StreamReader reader = new StreamReader(dataset_path + "ActorsDirectorsCodes_IMDB.tsv"))
+            {
+                await reader.ReadLineAsync();
+                while (!reader.EndOfStream)
+                {
+                    string line = await reader.ReadLineAsync();
+                    int zero = line.IndexOf("\t"), one = line.IndexOf("\t", zero + 1), two = line.IndexOf("\t", one + 1),
+                        three = line.IndexOf("\t", two + 1);
+                    string mov_id = line[..zero], chel_id = line[(one + 1)..two], categ = line[(two + 1)..three];
+                    if (!persons_for_async.ContainsKey(chel_id) || !films_id_name.ContainsKey(mov_id))
+                        continue;
+                    if (categ == "director")
+                    {
+                        persons_for_async[chel_id].director_movies_id.Add(mov_id);
+                    }
+                    else
+                    {
+                        persons_for_async[chel_id].actor_movis_id.Add(mov_id);
+                    }
+                    persons_for_async[chel_id].movies_id.Add(mov_id);
+                }
+            }
+            Console.WriteLine("Make people 2/2 done.");
         }
     }
 }
