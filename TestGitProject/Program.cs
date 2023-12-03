c048267d494112cd4db3419b36ec305020be5a2a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-12-03 11:02:34 +0300

    контур, вторая задача, превышение времени

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -8,16 +8,52 @@ namespace TestGitProject
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
             string[] data = Console.ReadLine().Split();
-            int n = Convert.ToInt32(data[0]), m = Convert.ToInt32(data[1]);
-            int ans = 0;
-            if (n < m)
-                ans = n + (n + 1) * 2;
-            if (n == m)
-                ans = n + m * 2;
-            if (n > m)
-                ans = m * 2 + (m + 1);
-            Console.WriteLine(ans);
+            int sum_left = 0, sum_right = Convert.ToInt32(data[n - 1]);
+            int index = 0;  // индекс, с которого начинается правая часть
+            bool flag = false;
+            for (int i = 0; i < n - 1; i += 1)
+                sum_left += Convert.ToInt32(data[i]);
+            index = n - 1;
+
+            if (n > 2)
+            {
+                while (index > 0)
+                {
+                    if (sum_left == sum_right)
+                    {
+                        flag = true;
+                        break;
+                    }
+                    index -= 1;
+                    int num = Convert.ToInt32(data[index]);
+                    sum_left -= num;
+                    sum_right += num;
+                }
+                if (!flag)
+                    Console.WriteLine(-1);
+                else
+                {
+                    for (int i = 0; i < n; i += 1)
+                    {
+                        Console.Write(data[i]);
+                        if (i + 1 == index)
+                            Console.Write("=");
+                        else
+                        {
+                            if (i + 1 != n)
+                                Console.Write("+");
+                        }
+                    }
+                }
+            } else
+            {
+                if (data[0] == data[1])
+                    Console.WriteLine($"{data[0]}={data[0]}");
+                else
+                    Console.WriteLine(-1);
+            }
         }
     }
 }
